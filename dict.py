da5c2df5cc02ed6e16524e8de1ed0bf35d07f8c7
nykonykonyko <nickosaj0@gmail.com>
2025-04-25 18:52:59 +0100

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,15 @@
+marvel = ["captain america","iron man"]
+marvel = {
+    "iron man" : ["flight", "super suit"],
+    "captain america" : "special shield",
+    "spiderman" : "spider-like abilities",
+    "address" : {
+        "home" : "self","office":"avengers headquarters",
+
+    },
+    "total" : 99
+}
+print(marvel)
+print(marvel["address"] )
+print(marvel["address"]["office"])
+print(marvel["iron man"][1])
